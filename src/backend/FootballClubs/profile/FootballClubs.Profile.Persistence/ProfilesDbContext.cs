554cc3e855c926e8b7f3e5ccff38debc8250576d
pmozola <mozola.pawel@gmail.com>
2025-10-21 12:33:50 +0200

    profile2

---
+++ src/backend/FootballClubs/profile/FootballClubs.Profile.Persistence/ProfilesDbContext.cs
@@ -0,0 +1,14 @@
+﻿using FootballClubs.Profile.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace FootballClubs.Profile.Persistence;
+
+public class ProfilesDbContext : DbContext
+{
+    public static string ConnectionStringSettingName = nameof(ProfilesDbContext);
+    public ProfilesDbContext(DbContextOptions<ProfilesDbContext> options) : base(options)
+    {
+    }
+    
+    public DbSet<UserProfile> UserProfiles { get; set; }
+}
